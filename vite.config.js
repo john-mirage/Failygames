964a4ce3ccc2486a9c9f5ad9d641c72efa77a35d
chaosdynamix <chaosdynamix@posteo.net>
2022-03-08 15:52:22 +0100

    feat(vite): add relative aliases

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@data': path.resolve(__dirname, 'src/data'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@scripts': path.resolve(__dirname, 'src/scripts'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+        },
+    },
+})
