ca1fa261313795fcfeef5d937af37fb0b411d117
chaosdynamix <chaosdynamix@posteo.net>
2022-03-08 22:00:19 +0100

    feat(vite): add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/Failygames/",
     resolve: {
         alias: {
             '@data': path.resolve(__dirname, 'src/data'),
