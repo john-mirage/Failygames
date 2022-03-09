3beb4c2105a00cafd7142db1adce1031528341b6
chaosdynamix <chaosdynamix@posteo.net>
2022-03-09 16:24:53 +0100

    feat(tablist): delete height fix

--- src/scripts/components/tab-list.js
+++
@@ -1,6 +0,0 @@
-const tabCarousel = document.getElementById("tab-list-carousel");
-
-export function setTabCarouselMinHeight() {
-    const tabCarouselHeight = tabCarousel.scrollHeight;
-    tabCarousel.style.minHeight = `${tabCarouselHeight}px`;
-}
